7978655d5f4b241758e42dc766015c6f285455ed
Hassan-Laraik <hassanlaraik@gmail.com>
2024-02-01 14:08:45 +0100

    Add Cours javascript

---
+++ Lecon-Js/script.js
@@ -0,0 +1,12 @@
+var 
+   nom =  "Hassani";
+   prenom  = "Ali";
+   service  = "\"Service\" d'Acceuil";
+
+   Nom_Complet = nom + "  " + prenom;
+ /*console.log(Nom_Complet)*/
+  var num1 = prompt('Entrez Une Valeur :','Votre valeur ici');
+   num1 = Number(num1);
+  num1 =  num1 + 1 ;
+
+   alert("valeur entrée est  : " + num1 );
